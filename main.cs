6a6f996118bc9ed7224864db60190c24dea61149
JMart25 <149201983+JMart25@users.noreply.github.com>
2024-05-24 16:18:49 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Oh, well imagine \\");
+        Console.WriteLine("As I'm pacing the pews in a church corridor \\");
+        Console.WriteLine("And I can't help but to hear \\");
+        Console.WriteLine("No, I can't help but to hear an exchanging of words...");
+    }
+}
